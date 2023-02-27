d36264b11b53c5d78239c87716d974830253ff12
shreekhand.wankhede <shreekhand.wankhede@gmail.com>
2023-02-27 21:28:36 +0530

    Spring Cloud Circuit breaker with Resilience4j as the underlying implementation. initial commit

---
+++ src/main/java/com/example/springbootcircuitbreakerexample/SpringBootCircuitBreakerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcircuitbreakerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCircuitBreakerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCircuitBreakerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootcircuitbreakerexample/SpringBootCircuitBreakerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcircuitbreakerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCircuitBreakerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
